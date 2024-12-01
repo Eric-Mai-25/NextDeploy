dd724c7fb79e256a6d955151276757adffcbd48d
Eric-Mai-25 <ericmai2@gmail.com>
2024-11-30 16:10:43 -0800

    convert to my own website

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({searchParams}: {
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup,<br /> Connect With Entreprenurs</h1>
+        <h1 className="heading">State your mind<br /> Your One stop for gaming news</h1>
         <p className="sub-heading !max-w-3xl"> Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
         <SearchForm query={query}/>
       </section>
